7e3eb9ddc51f09c7d20bb89382bc7e803d6d5f7d
tahamudassar <118525303+tahamudassar@users.noreply.github.com>
2025-01-17 15:20:35 +0500

    persistent storage done
    error attached in log file

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,26 @@
 // src/app/store.js
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import cartReducer from './slices/cartSlice';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  // ...other reducers
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
