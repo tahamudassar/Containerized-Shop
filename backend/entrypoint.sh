589f0374d210225a56d27151c3b7b818a042cf38
tahamudassar <118525303+tahamudassar@users.noreply.github.com>
2025-02-17 16:23:47 +0500

    code cleaned up

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,16 @@
 python manage.py migrate
 
 # Create superuser if it doesn't exist
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-User.objects.filter(username='admin').exists() or \
-User.objects.create_superuser('admin', 'admin@example.com', '12345678')" | python manage.py shell
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(username="admin", email="admin@admin.com", password="12345678")
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+EOF
 
 # Start the server
 python manage.py runserver 0.0.0.0:8000
